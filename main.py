d3b9d5cc9798d2e9a9125721240ce523cbd7bc66
Tomáš Turecký <t_turecky@utb.cz>
2025-02-17 13:29:43 +0100

    Added not working code

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def sphere(x):
-    print("test")
+def sphere(k):
+    X = []
+    for i in range(k):
+        X.append(np.linspace(-5.12,5.12,10000))
+    suma = 0
+    for x in X:
+        suma = suma + x**2
+    y = suma
+    ax = plt.axes(projection='3d')
+    ax.plot3D(X[0],X[1],y)
+    plt.show()
+    print(y)
+
 
 
 sphere(2)
