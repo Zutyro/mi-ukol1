f3e0201fff9b17e7e80f15a31ddd7acbd192085b
Zutyro <t_turecky@outlook.com>
2025-02-18 10:46:05 +0100

    Edited sphere function

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ def sphere(k):
     X = []
     for i in range(k):
         X.append(np.linspace(-5.12,5.12,10000))
+    x1,x2 = np.meshgrid(X[0],X[1])
     suma = 0
     for x in X:
         suma = suma + x**2
     y = suma
     ax = plt.axes(projection='3d')
-    ax.plot3D(X[0],X[1],y)
+    ax.plot3D(x1,x2,y)
     plt.show()
     print(y)
 
