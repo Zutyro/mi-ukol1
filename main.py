ac5b71ada8b87bdda598b033e216f3b397799201
Tomáš Turecký <t_turecky@utb.cz>
2025-02-26 19:07:01 +0100

    Added Schwefel, Dixon-Price and Rosenbrock functions.

--- main.py
+++ main.py
@@ -86,5 +86,134 @@ def trid(k):
     plt.plot(lowest_records_swapped[1],lowest_records_swapped[0],drawstyle="steps-post")
     plt.show()
 
+def schwefel(k):
+    x_list = []
+    for i in range(k):
+        x_list.append(np.linspace(-500, 500, 500))
+    # # Vizualizace
+    # x_mesh = (np.meshgrid(*x_list))
+    # suma1 = 0
+    # for x in x_mesh:
+    #     suma1 = suma1 + x * np.sin(np.sqrt(np.abs(x)))
+    # y = 418.9829*k - suma1
+    # ax = plt.axes(projection='3d')
+    # ax.plot_surface(x_mesh[0], x_mesh[1], y)
+    # plt.show()
+    lowest_records = []
+    lowest_coords = []
+    for i in range(k * 10000):
+        coords = []
+        suma1 = 0
+        for x in x_list:
+            coords.append(random.choice(x))
+        for x in coords:
+            suma1 = suma1 + x * np.sin(np.sqrt(np.abs(x)))
+        y = 418.9829*k - suma1
+        if i == 0:
+            lowest_records.append([y, i + 1])
+        if lowest_records[len(lowest_records) - 1][0] > y:
+            lowest_records.append([y, i + 1])
+            lowest_coords = coords
+    lowest_records_swapped = np.swapaxes(lowest_records, 0, 1)
+    lowest_coords_concated = ''
+    for i, coord in enumerate(lowest_coords):
+        if i % 5 == 0 and i != 0:
+            lowest_coords_concated = lowest_coords_concated + '\n'
+        lowest_coords_concated = lowest_coords_concated + str(coord) + ', '
+    plt.title(f'Schwefel in {k}D (maxFES = {k * 10000})\nBest solution: {lowest_records[len(lowest_records) - 1][0]}'
+              f', in: \n{lowest_coords_concated}')
+    plt.plot(lowest_records_swapped[1], lowest_records_swapped[0], drawstyle="steps-post")
+    plt.show()
+
+def dixonprice(k):
+    x_list = []
+    for i in range(k):
+        x_list.append(np.linspace(-10, 10, 500))
+    # # Vizualizace
+    # x_mesh = (np.meshgrid(*x_list))
+    # suma1 = 0
+    # for i,x in enumerate(x_mesh):
+    #     if i == 0:
+    #         continue
+    #     suma1 = suma1 + (i+1) * (2*(x**2) - x_mesh[i-1])**2
+    # y = (x_mesh[0]-1)**2 + suma1
+    # ax = plt.axes(projection='3d')
+    # ax.plot_surface(x_mesh[0], x_mesh[1], y)
+    # plt.show()
+    lowest_records = []
+    lowest_coords = []
+    for i in range(k * 10000):
+        coords = []
+        suma1 = 0
+        for x in x_list:
+            coords.append(random.choice(x))
+        for j, x in enumerate(coords):
+            if j == 0:
+                continue
+            suma1 = suma1 + (j + 1) * (2 * (x ** 2) - coords[j - 1]) ** 2
+        y = (coords[0]-1)**2 + suma1
+        if i == 0:
+            lowest_records.append([y, i + 1])
+        if lowest_records[len(lowest_records) - 1][0] > y:
+            lowest_records.append([y, i + 1])
+            lowest_coords = coords
+    lowest_records_swapped = np.swapaxes(lowest_records, 0, 1)
+    lowest_coords_concated = ''
+    for i, coord in enumerate(lowest_coords):
+        if i % 5 == 0 and i != 0:
+            lowest_coords_concated = lowest_coords_concated + '\n'
+        lowest_coords_concated = lowest_coords_concated + str(coord) + ', '
+    plt.title(f'Dixon-price in {k}D (maxFES = {k * 10000})\nBest solution: {lowest_records[len(lowest_records) - 1][0]}'
+              f', in: \n{lowest_coords_concated}')
+    plt.plot(lowest_records_swapped[1], lowest_records_swapped[0], drawstyle="steps-post")
+    plt.show()
+
+def rosenbrock(k):
+    x_list = []
+    for i in range(k):
+        x_list.append(np.linspace(-10, 10, 500))
+    # # Vizualizace
+    # x_mesh = (np.meshgrid(*x_list))
+    # suma1 = 0
+    # for i,x in enumerate(x_mesh):
+    #     if i == len(x_mesh)-1:
+    #         continue
+    #     suma1 = suma1 + (100*(x_mesh[i+1]-x**2)**2 + (x-1)**2)
+    # y = suma1
+    # ax = plt.axes(projection='3d')
+    # ax.plot_surface(x_mesh[0], x_mesh[1], y)
+    # plt.show()
+    lowest_records = []
+    lowest_coords = []
+    for i in range(k * 10000):
+        coords = []
+        suma1 = 0
+        for x in x_list:
+            coords.append(random.choice(x))
+        for j, x in enumerate(coords):
+            if j == len(coords) - 1:
+                continue
+            suma1 = suma1 + (100 * (coords[j + 1] - x ** 2) ** 2 + (x - 1) ** 2)
+        y = suma1
+        if i == 0:
+            lowest_records.append([y, i + 1])
+        if lowest_records[len(lowest_records) - 1][0] > y:
+            lowest_records.append([y, i + 1])
+            lowest_coords = coords
+    lowest_records_swapped = np.swapaxes(lowest_records, 0, 1)
+    lowest_coords_concated = ''
+    for i, coord in enumerate(lowest_coords):
+        if i % 5 == 0 and i != 0:
+            lowest_coords_concated = lowest_coords_concated + '\n'
+        lowest_coords_concated = lowest_coords_concated + str(coord) + ', '
+    plt.title(f'Rosenbrock in {k}D (maxFES = {k * 10000})\nBest solution: {lowest_records[len(lowest_records) - 1][0]}'
+              f', in: \n{lowest_coords_concated}')
+    plt.plot(lowest_records_swapped[1], lowest_records_swapped[0], drawstyle="steps-post")
+    plt.show()
+
+
 # trid(20)
 # sphere(5)
+# schwefel(20)
+# dixonprice(5)
+# rosenbrock(20)
